7602b51c7e115b739095d592913faf5ee4fd65a8
Lia <122105453+CanadianPS1@users.noreply.github.com>
2026-03-09 11:03:41 -0600

    fixed the bug

--- obj/Debug/net10.0-windows/ET.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/ET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1689050bec9bcab570752ce981b8125e7a544d0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7cd50d46cb6fd91ab4be10825b811d8a008bb7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
